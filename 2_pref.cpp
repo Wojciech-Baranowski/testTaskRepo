536f038e70cf0abdb25c22ad5edd3695ff44924f
simmondobber <wojciechfbaranowski@gmail.com>
2024-02-04 20:36:12 +0100

    2_pref

--- 2_pref.cpp
+++ 2_pref.cpp
@@ -3,25 +3,27 @@
 
 using namespace std;
 
-int n, k, a, b, s;
+int n, k, a, b;
 int tab[MAXN];
 int pref[MAXN];
 
 int main() {
-
 	cin >> n >> k;
 	for(int i = 0; i < n; i++) {
 		cin >> tab[i];
+	}
+	pref[0] = tab[0];
+	for(int i = 1; i < n; i++) {
+		pref[i] = pref[i - 1] + tab[i];
 	}
 	for(int i = 0; i < k; i++) {
 		cin >> a >> b;
 		a--;
 		b--;
-		s = 0;
-		for(int j = a; j <= b; j++) {
-			s += tab[j];
+		if(a == 0) {
+			cout<<pref[b]<<"\n";
+		} else {
+			cout<<pref[b] - pref[a - 1]<<"\n";
 		}
-		cout<<s<<"\n";
 	}
-	return 0;
 }
