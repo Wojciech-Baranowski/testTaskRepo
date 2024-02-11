13be4eb4c8cae3344e60766652917abb8deb4667
simmondobber <wojciechfbaranowski@gmail.com>
2024-02-11 22:27:42 +0100

    6_bins

--- 6_bins.cpp
+++ 6_bins.cpp
@@ -4,21 +4,28 @@ using namespace std;
 
 long long t, n, m, k;
 
+int binSearch(int a, int b) {
+	if(b - a <= 1) {
+		if((t - a) * n + a * m >= k) {
+			return a;
+		} else {
+			return b;
+		}
+	}
+	int p = (a + b) >> 1;
+	if((t - p) * n + p * m >= k) {
+		return binSearch(a, p);
+	} else {
+		return binSearch(p + 1, b);
+	}
+}
+
 int main() {
 	cin >> t >> n >> m >> k;
 	if(t * m < k) {
 		cout<<"POMOC"<<"\n";
 		return 0;
 	}
-	if(t * n >= k) {
-		cout<<0<<"\n";
-		return 0;
-	}
-	for(int i = 0; i < t; i++) {
-		if(((t - i) * n + i * m < k) && ((t - i - 1) * n + (i + 1) * m >= k)) {
-			cout<<i + 1<<"\n";
-			return 0;
-		}
-	}
+	cout<<binSearch(0, t + 1)<<"\n";
 	return 0;
 }
