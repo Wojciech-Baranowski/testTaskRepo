f28166e53c23db91a33e27bc1361c3910fa66487
Jakub Grabowski <grajak06@wp.pl>
2024-03-08 16:46:11 +0100

    skoncze to w domu

---
+++ 3_pref2d.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, k;
+int M[n][m];
+int P[n][m];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m >> k;
+	for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) cin >> M[i][j];
+	for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) {
+		P[i][0] = M[i][0];
+		P[0][j] = M[0][j];
+	}	
+	for (int i = 1; i <= k; i++) {
+		int ax, ay, bx, by;	
+	}	
+}
