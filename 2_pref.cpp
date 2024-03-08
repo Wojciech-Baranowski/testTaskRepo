1600cb8ad412dc1bd33673c55f57d6968a6d49ff
Jakub Grabowski <grajak06@wp.pl>
2024-03-08 16:48:25 +0100

    male poprawki

--- 2_pref.cpp
+++ 2_pref.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int n, k;
+int C[n];
+int P[n];
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n, k;
 	cin >> n >> k;
-	int C[n];
 	for (int i = 0; i < n; i++) cin >> C[i];
-	int P[n];
 	P[0] = C[0];
 	for (int i = 1; i < n; i++) P[i] = P[i-1] + C[i];
 	for (int i = 1; i <= k; i++) {
