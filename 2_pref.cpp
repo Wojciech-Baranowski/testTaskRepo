a64e38a35dd870f8d14929b1f3da5bffec9c4715
Jakub Grabowski <grajak06@wp.pl>
2024-03-08 21:21:07 +0100

    jednak tak nie dziala

--- 2_pref.cpp
+++ 2_pref.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int n, k;
-int C[n];
-int P[n];
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
 	cin >> n >> k;
+	int C[n];
+	int P[n];
 	for (int i = 0; i < n; i++) cin >> C[i];
 	P[0] = C[0];
 	for (int i = 1; i < n; i++) P[i] = P[i-1] + C[i];
--- 3_pref2d.cpp
+++ 3_pref2d.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int n, m, k;
-int M[n][m];
-int P[n][m];
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
 	cin >> n >> m >> k;
+	int M[n][m];
+	int P[n][m];
 	for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) cin >> M[i][j];
 	for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) {
 		P[i][0] = M[i][0];
