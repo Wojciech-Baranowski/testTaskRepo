3c2a5189283d132eebfcb8f1a85e404c61bfed80
Maciej Tuleja <maciej.tuleja6@gmail.com>
2024-03-09 15:04:40 +0100

    3_pref2d done

--- zad/3_pref2d.cpp
+++ zad/3_pref2d.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 int tab[1000][1000];
 int p[1000][1000];
+
 int n;
 int m;
 int k;
+
 int ax;
 int ay;
 int bx;
@@ -13,11 +15,9 @@ int by;
 
 int prefixSum(int ax, int ay, int bx, int by)
 {
-		cout << "done" << endl;
 		int sum = 0;
 		for(int i = ay; i <= by;i++)
 		{
-			cout << i << "/" << bx << endl;
 			sum += (p[i][bx] - p[i][ax-1]);
 		}
 		return sum;
@@ -28,7 +28,7 @@ int main()
 	cin >> n;
 	cin >> m;
 	cin >> k;
-	cout << n << "/" << m << endl;
+	
 	for(int i = 0; i < n;i++) 
 	{ 
 		for(int j = 0; j < m;j++)
@@ -37,12 +37,9 @@ int main()
 		}
 	}
 	
-	cout << "done3" << endl;
-	for(int i = 0; i < n;i++)
-	{
-		p[n][0] = tab[n][0];
-	}
-	for(int i = 1; i < n;i++) 
+	
+	for(int i = 0; i < n;i++) { p[i][0] = tab[i][0]; }
+	for(int i = 0; i < n;i++) 
 	{ 
 		for(int j = 1; j < m;j++)
 		{
@@ -56,9 +53,8 @@ int main()
 		cin >> ay;
 		cin >> bx;
 		cin >> by;
-		
-		cout << "done2" << endl;
-		cout << prefixSum(ax-1,ay-1,bx-1,by-1) << endl;
+
+		cout << prefixSum(ax-1,ay-1,bx-1,by-1) << "\n";
 	}
 	
 	return 0;
