3a92762ff5f3a24e40c75d0338f4b955433ddab5
Jakub Grabowski <grajak06@wp.pl>
2024-03-12 19:56:47 +0100

    Tutaj też zastosuję swój trik

--- 2_pref.cpp
+++ 2_pref.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int n, k;
-int C[n], P[n];
+int C[MAXN], P[MAXN];
 
 int main() {
 	ios_base::sync_with_stdio(0);
@@ -14,21 +14,15 @@ int main() {
 		cin >> C[i];
 	}
 
-	P[0] = C[0];
-	for (int i = 1; i < n; i++) {
-		P[i] = P[i-1] + C[i];
+	P[0] = 0;
+	P[1] = C[0];
+	for (int i = 2; i < n + 1; i++) {
+		P[i] = P[i-1] + C[i-1];
 	}
 
 	for (int i = 0; i < k; i++) {
 		int a, b;
 		cin >> a >> b;
-		a--;
-		b--;
-		if (a == 0) {
-			cout << P[b] << "\n";
-		}
-		else {
-			cout << P[b] - P[a - 1] << "\n";
-		}
+		cout << P[b] - P[a - 1] << "\n";
 	}
 }
