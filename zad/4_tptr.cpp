f40649f9f3559e76ea39e0e8686f8876812f7f47
Maciej Tuleja <maciej.tuleja6@gmail.com>
2024-03-15 16:43:50 +0100

    4_tptr.cpp

---
+++ zad/4_tptr.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int tab[1000000];
+
+int a,b;
+
+int sum;
+int maxSum;
+
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> tab[i];
+	}
+	
+	a = 0;
+	b = 0;
+	sum = tab[0];
+	
+	while (a < n - 1)
+	{
+		if (b == n)
+		{
+			sum -= tab[a];
+			a++;
+			if (maxSum < sum)
+			{
+				maxSum = sum;
+			}
+		}
+		if (sum < 0)
+		{
+			if (a == b)
+			{
+				b++;
+				sum += tab[b];
+			}
+			sum -= tab[a];
+			a++;
+			if (maxSum < sum)
+			{
+				maxSum = sum;
+			}
+			continue;
+		}
+		if (sum >= 0)
+		{
+			if (b < n)
+			{
+				b++;
+				sum += tab[b];
+			}
+			if (maxSum < sum)
+			{
+				maxSum = sum;
+			}
+			continue;
+		}
+	}
+	cout << maxSum << "\n";
+}
