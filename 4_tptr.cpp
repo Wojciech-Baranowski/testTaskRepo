9c23337821dd041ac1df15a755709b613d6cae11
Filip Krakus <fiszekkrakus@gmail.com>
2024-03-16 15:34:33 +0100

    update to 4_tptr

--- 4_tptr.cpp
+++ 4_tptr.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int *a, *b, n, tab[1000000], p[1000000], mocarz;
+int a=0, b=0, n, tab[1000000], p[1000000], mocarz=0;
 int main() {
 	cin>>n;
 	p[0] = 0;
@@ -8,14 +8,14 @@ int main() {
 		cin>>tab[i];
 		p[i+1] = p[i] + tab[i];
 	}
-	a = &tab[0];
-	b = &tab[0];
-	mocarz = 0;
-	while (a != n) {
-		if (p[*b+1] - p[*a] >= mocarz)
-			mocarz = p[*b+1] - p[*a];
-		if ( p[*b+1] - p[*a] >= 0) {
+	while (b != n) {
+		if (p[b+1] - p[a] >= mocarz)
+			mocarz = p[b+1] - p[a];
+		if ( p[b+1] - p[a] >= 0) {
 			b++;
+		}
+		else if (a<b) {
+			a++;
 		}
 		else {
 			a++;
