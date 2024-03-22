ca0d2254a586ffbdb86f51f647f930834a6ae27a
Mikolaj Witkowski <mikolajwitkowski27@gmail.com>
2024-03-22 16:48:10 +0100

    6_bins

---
+++ 6_bins.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int t, n, m, k; 
+
+int binSearch() {
+	if (m * t < k) {
+		return 0; 
+	}
+	int a = 0, b = m; 
+	else {
+		if (a == b) {
+			if (a = min) {
+				return a; 
+			}
+			else {
+				return -1; 
+			}
+		 }
+		 int srodek = (a + b)>>1; 
+		 if ()
+	}
+}
+int main() {
+	cin >> t >> n >> m >> k;	//>>1 przesunięcie bitowe 
+	cout << binSearch() << "\n"; 
+	return 0; 
+}
