a7d7df0fef6f59eea507162df66b0cbd111a09c4
Maciej Tuleja <maciej.tuleja6@gmail.com>
2024-04-08 09:27:25 +0200

    8_cups

---
+++ zad/8_cups.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int errors[1000];
+
+int maxIndex = 0;
+int n;
+
+int main()
+{
+	for (int i = 0; i < 1000; i++)
+	{
+		errors[i] = 0;
+	}
+	
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		string temp;
+		cin >> temp;
+		
+		int index = stoi(temp.substr(12, 14));
+		errors[index]++;
+	}
+	
+	for (int i = 0; i < 1000; i++)
+	{
+		if (errors[i] > errors[maxIndex])
+		{
+			maxIndex = i;
+		}	
+	}
+	
+	cout << "ERROR" << (maxIndex + 1000000000) << "\n";
+	return 0;
+}
